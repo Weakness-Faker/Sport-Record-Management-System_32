df08b5f4de14c532b5a01bf5a686975323dad291
Weakness-Faker <ab060423@qq.com>
2025-11-30 21:58:02 +0800

    Update DBConnection.java

--- DBConnection.java
+++ DBConnection.java
@@ -3,10 +3,11 @@ import java.sql.*;
 
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/fitness_tracker";
-    private static final String USER = "root";       // 改成你的MySQL用户名
-    private static final String PASSWORD = "060423"; // 改成你的MySQL密码！（重点！）
+    private static final String USER = "root";       
+    private static final String PASSWORD = "060423"; 
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
+
 }
